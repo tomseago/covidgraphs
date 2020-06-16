8ae793ee08c3009f46630bce0af2fdcdfbf6e906
Tom Seago <tom@tomseago.com>
2020-06-16 01:54:54 -0700

    Adding updated data and filtering improvements

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # covidgraphs
 Some graph experiments using covid data
+
+## Build
+
+Run `gulp` from the root. This builds pug files from views and stylus files from stylus into the root directory.
+
+## Running
+
+`yarn start` will start the static webserver via `node server.js` which is super simple and just runs on port 3000.
+
+At the moment the default index.html is lame. You want to go to
+
+    http://localhost:3000/statesSurface.html
+    
+## Data
+
+All the data that is used sits in static json files in the data/ directory. The `versions.json` file identifies available versions of the data. It is an array of strings naming sub directories. The `regions.json` file identifies regions which will be present in the sub directories.
+
+Inside a version subdirectory there is a json file per each region.
+
+The data is from https://covid19.healthdata.org/
+The most recent .zip file can be downloaded from this page: http://www.healthdata.org/covid/data-downloads and should be named ihme-covid19.zip
+
+These data files are CSVs and the zip file will look something like
+
+     creating: 2020_06_13/
+    inflating: 2020_06_13/Hospitalization_all_locs.csv  
+    inflating: 2020_06_13/IHME_COVID_19_Data_Release_Information_Sheet.pdf  
+    inflating: 2020_06_13/Summary_stats_all_locs.csv
+    
+To create the json files, unzip the file in the root directory and then run the injest.js file supplying the name of the version file. In this case that would look like
+ 
+    node injest.js 2020_06_13   
+
+This will add the new data to the data directory in the proper format.
+
--- js/states.js
+++ js/states.js
@@ -1,3 +1,4 @@
+let selectedStates = [];
 
 loadCommon()
     .then(() => {
@@ -13,9 +14,15 @@ loadCommon()
         const all = [];
         stateNames.forEach((name) => {
             all.push(loadData(latestVersion, name));
+
+            // Also setup a select checkbox for it
+            addStateCheckbox(name);
         });
 
         return Promise.all(all);
+    })
+    .then(() => {
+        return spawnUpdatePlot();
     })
     .catch((e) => {
         console.log(e);
@@ -24,11 +31,26 @@ loadCommon()
 
 function changeMetric(event) {
     const val = event.target.value;
-    console.log("changeMetric(",event,") val=",val);
+    console.log("changeMetric(", event, ") val=", val);
 
-    // const metricName = `${val}_mean`;
-    const metricName = val;
-    
+    spawnUpdatePlot();
+}
+
+function spawnUpdatePlot() {
+    return new Promise((resolve, reject) => {
+        updatePlot();
+        resolve();
+    }).catch((e) => {
+        console.error(e);
+    });
+}
+
+async function updatePlot() {
+    const selectEl = document.getElementById("metricSelect");
+
+    const metricName = selectEl.value;
+    console.log("metricName=",metricName);
+
     const ix = fieldNames.indexOf(metricName);
     if (ix === -1) {
         console.log(`Could not find ${metricName}`);
@@ -40,7 +62,33 @@ function changeMetric(event) {
     // Create the new matrix for this metric
     dataByDay = {};
 
-    stateNames.forEach((state) => {
+    const checkedNames = {};
+    const els = document.getElementsByClassName("state-cb-input");
+    // console.log("els ", els);
+    for(let key in els) {
+        const el = els[key];
+
+        if (el.checked) {
+            checkedNames[el.id.substring(4)] = true;
+        }
+    }
+
+    // Now order them based on the selected thing
+    const sortSelectEl = document.getElementById("sortSelect");
+    const ssVal = sortSelectEl.value;
+    let allSorted = sortedStateNames[ssVal] || sortedStateNames.alpha;
+
+    selectedStates = [];
+    for(let i = 0; i < allSorted.length; i += 1) {
+        const name = allSorted[i];
+
+        if (checkedNames[name]) {
+            selectedStates.push(name);
+        }
+    }
+    console.log("Sorted by ", ssVal, " => ", selectedStates);
+
+    selectedStates.forEach((state) => {
         const d = $data[state];
 
         d.forEach((dRow) => {
@@ -77,7 +125,7 @@ function updateTable() {
     const rowKeys = Object.keys(dataByDay);
     rowKeys.forEach((day) => {
 
-        if (day < "2020-02-01" || day > "2020-06-01") {
+        if (day < "2020-02-01" || day > "2020-07-01") {
             // console.log("Ignoring ", day);
             return;
         }
@@ -88,7 +136,7 @@ function updateTable() {
         const row = [];
 
         // Now go by states
-        stateNames.forEach((state) => {
+        selectedStates.forEach((state) => {
             const val = dayData[state] || 0;
             row.push(val);
         })
@@ -103,19 +151,47 @@ function updateTable() {
 }
 
 function tableToPlotly() {
+    // For colors https://plotly.com/javascript/colorscales/
+
     const data = [{
         y: rowLabels,
-        x: stateNames,
+        x: selectedStates,
         z: dataInRows,
         type: "surface",
+        //     // Greys,YlGnBu,Greens,YlOrRd,Bluered,RdBu,Reds,Blues,Picnic,Rainbow,Portland,Jet,Hot,Blackbody,Earth,Electric,Viridis,Cividis.
+        // colorscale: "Rainbow",
+        colorscale: "Jet",
     }];
 
     const layout = {
-        width: 1200,
-        height: 900,
+        // width: 1200,
+        // height: 900,
+        width: window.innerWidth - 200,
+        height: window.innerHeight,
+
+        scene: {
+            aspectratio: {
+                x: 2,
+                y: 1,
+                z: 1,
+            },
+
+            xaxis: {
+                tickmode: "auto",
+                nticks: 50,
+            },
+        },
+
+        // autosize: false,
+        // // https://plotly.com/javascript/reference/#layout-coloraxis-colorscale
+        // coloraxis: {
+        //     autocolorscale: false,
+        //     colorscale: "rainbow",
+        // },
     }
 
     Plotly.newPlot("plot", data, layout);
+    // Plotly.newPlot("plot", data, layout, {responsive: true});
 }
 
 
@@ -123,6 +199,10 @@ function updateFields() {
     const el = document.getElementById("metricSelect")
 
     fieldNames.forEach((name) => {
+        if (name === "V1" || name === "location_name" || name === "date") {
+            return;
+        }
+
         const opt = document.createElement("option");
         opt.value = name;
         opt.textContent = name;
@@ -131,3 +211,100 @@ function updateFields() {
     });
 
 }
+
+
+function addStateCheckbox(name) {
+    const field = document.createElement("div");
+    field.className = "state-cb"
+
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    const n = `stcb${name}`;
+    input.id = n;
+    input.name = n;
+    input.checked = true;
+    input.className = "state-cb-input";
+    input.onchange = stcbChange;
+
+    field.appendChild(input);
+
+    const label = document.createElement("label");
+    label.htmlFor = n;
+    label.textContent = name;
+
+    field.appendChild(label);
+
+    const list = document.getElementById("stateList");
+    list.appendChild(field);
+}
+
+
+function stcbChange(event) {
+    console.log("stcbChange for ", event.target.id, " checked=", event.target.checked);
+
+    spawnUpdatePlot();
+}
+
+function changeSort(event) {
+    console.log("Change sort");
+    spawnUpdatePlot();
+}
+
+/////////////
+
+function selSome(which) {
+    console.log("selSome(",which,")");
+
+    // Get our sorted list
+    const sortSelectEl = document.getElementById("sortSelect");
+    const ssVal = sortSelectEl.value;
+    let allSorted = sortedStateNames[ssVal] || sortedStateNames.alpha;
+
+    const toCheck = {};
+    for(let i = 0; i < allSorted.length; i += 1) {
+        const name = allSorted[i];
+
+        // Written this way in case we want to make it more complicated
+        // in the future
+        switch(which) {
+        case "top10":
+            if (i<10) {
+                toCheck[name] = true;
+            }
+            break;
+
+        case "bot10":
+            if (i > allSorted.length - 11) {
+                toCheck[name] = true;
+            }
+            break;
+
+        default: // all
+            toCheck[name] = true;
+        }
+    }
+
+    // Now iterate all the checked elements
+    const els = document.getElementsByClassName("state-cb-input");
+    for(let i = 0; i < els.length; i += 1) {
+        const el = els[i];
+
+        const id = el.id.substring(4);
+        // Hopefully this doesn't fire events right?
+        el.checked = toCheck[id];
+    }
+
+    // Now we do just one event yeah?
+    spawnUpdatePlot();
+}
+
+
+///////////////
+
+// Watch resize stuff
+window.addEventListener("resize", (event) => {
+    console.log("== Resize == ", event);
+
+    // Redo this guy
+    tableToPlotly();
+});
